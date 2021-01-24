8dae2af4121b093a9be991e4bd17d5ac23357ac6
Diggory Laycock <nospam@diggory.net>
2021-01-24 20:26:05 +0000

    made prop public

--- Sources/SimpleSwiftPackage/SimpleSwiftPackage.swift
+++ Sources/SimpleSwiftPackage/SimpleSwiftPackage.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 @objcMembers public class SwiftClass : NSObject {
-    @objc var text = "Foo"
+    @objc public let text = "Foo"
     
     public func sayHello() {
         print(text)
